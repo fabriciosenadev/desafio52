3fe85e28a19f47ef61aa03a78ba2d8051e8e5e86
Fabricio Sena <fabricior.sena@outlook.com>
2020-01-31 18:57:29 -0300

    removed bugs and added style to table

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -1,38 +1,85 @@
+const btnCalculate = document.querySelector('#calculate');
+var amount = document.querySelector("#amount");
+// var table = "";
+
+btnCalculate.addEventListener('click', calculate);
+
 /**
  * Function calculate
  */
 function calculate(){
     
-    var amount = document.getElementById("amount").value;
-    var amountFixed = amount.replace(",",".");
-    var amountBase = parseFloat(amountFixed);
-    var table = "";
-    // var htmlTable;
-    var safe = deposit = 0;
-    var firstWeek = 1;
-    var totalWeeks = 52;
+    let amountFixed = amount.value.replace(",",".");
+    let amountBase = parseFloat(amountFixed);
+    let safe = deposit = 0;
+    let week = 1;
+    let totalWeeks = 52;
     
     if(amountBase){
         
         clearContent();
         
-        for(week = firstWeek; week <= totalWeeks; week++) {
+        for(let counter = 1; counter <= 55; counter++) 
+        {
+            
+            let htmlTable = selectTable(week);            
+            let table = document.querySelector(`#${htmlTable}`);
+            table.setAttribute('border', '1');
+            
+            let tbody = document.createElement('tbody');
 
-            htmlTable = selectTable(week);
+            var tr = document.createElement('tr');
+            for (let count = 0; count < 3; count++)
+            {
+                if (counter == 1 || counter == 19 || counter == 37) {
+                    let th = document.createElement('th');
+                    
+                    switch (count)
+                    {
+                        case 0:
+                            th.innerHTML = 'Semana';
+                            break;
+                        case 1:
+                            th.innerHTML = "Depositar";
+                            break;
+                        case 2:
+                            th.innerHTML = "Guardado";
+                            break;
+                    }
+                    
+                    tr.appendChild(th);
+
+                } else {
+                    
+                    deposit += amountBase;
+                    safe += deposit;
+                    
+                    let td = document.createElement('td');
+                    
+                    switch (count)
+                    {
+                        case 0:
+                            td.innerHTML = week;
+                            break;
+                        case 1:
+                            td.innerHTML = deposit.toFixed(2);
+                            break;
+                        case 2:
+                            td.innerHTML = safe.toFixed(2);
+                            break;                                    
+                    }
+                    
+                    tr.appendChild(td);
+                
+                }
+            }   
+            tbody.appendChild(tr);
+            table.appendChild(tbody);
             
-            table = document.getElementById(htmlTable).innerHTML;
-            
-            deposit += amountBase;
-            safe += deposit 
-            
-            table += "<tr>";
-            
-            table +=    "<td width='40px'>"+ week +"</td>" + "<td width='60px'>" + deposit.toFixed(2) + "</td> <td width='60px'>" + safe.toFixed(2) +" </td>";
-            
-            table += "<td>";
-            
-            document.getElementById(htmlTable).innerHTML = table;
-            
+            if(counter > 1) {
+                week++;
+                counter == 19 || counter == 37 ? week--: null;
+            }
         }
         
     }
@@ -47,7 +94,7 @@ function calculate(){
 function clearContent (content) {
     var clearTable = 1;
     
-    if(content){
+    if(content || content == undefined){
         content = "";
     }
 
@@ -74,15 +121,13 @@ function clearContent (content) {
  * @param {string} week 
  */
 function selectTable (week) {
-    var table;
+    let table;
 
     if (week <= 17) {
         table = "table1";
-    }
-    if ( (week >= 18) && (week <=34) ) {
+    }else if (week <= 34) {
         table = "table2";
-    }
-    if (week >= 35) {
+    } else {
         table = "table3";
     }
     
